05f0095d0914e6f72c92924cc9dcc3948b1cb518
guyuri <yulrhymz@gmail.com>
2022-02-08 20:15:26 +0900

    [#11] redux/saga 설정

--- src/components/contents/Items.js
+++ src/components/contents/Items.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { stateItems, actionsItems } from 'store/items/itemsSlice';
-// import actionsItems from 'store/items/itemsActions.js';
+import { stateItems } from 'store/items/itemsSlice';
+import actionsItems from 'store/items/itemsActions.js';
 
 const Items = () => {
   const dispatch = useDispatch();
--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import { all } from 'redux-saga/effects';
 import itemsReducer from './items/itemsSlice.js';
+import { takeEveryItems } from './items/itemsActions.js';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export default configureStore({
   reducer: {
     items: itemsReducer,
   },
+  middleware: [sagaMiddleware],
 });
 
-// import createSagaMiddleware from 'redux-saga';
-// import { all } from 'redux-saga/effects'
-// import itemsReducer from './items/itemsSlice.js';
-// import { takeEveryItems } from './items/itemsActions.js';
-
-// const sagaMiddleware = createSagaMiddleware();
-
-// export default configureStore({
-//   reducer: {
-//     items: itemsReducer
-//   },
-//   middleware : [ sagaMiddleware ]
-// });
-
-// sagaMiddleware.run(function* () {
-//   yield all([takeEveryItems()]);
-// });
+sagaMiddleware.run(function* () {
+  yield all([takeEveryItems()]);
+});
